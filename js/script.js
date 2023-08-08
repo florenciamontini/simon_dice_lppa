3d7eb14d7ffe539a7c7162c25d540558b273c07d
florenciamontini <flor_montini@hotmail.com>
2023-08-07 21:18:51 -0300

    validacion nombre inicial

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 var secuencia = [];
 const nombre = document.getElementById("nombre");
+const formulario = document.querySelector("form");
+
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (!formulario.checkValidity()) { //valida si el form es correcto
+        event.preventDefault();
+    } else {
+        formulario.reset();
+    }
+});
+
+nombre.addEventListener("input", function (event) {
+    console.log(nombre.value);
+    if (nombre.value.length >= 3) {
+        nombre.setCustomValidity("");
+    } else {
+        nombre.setCustomValidity("El nombre debe tener al menos tres letras");
+    }
+});
